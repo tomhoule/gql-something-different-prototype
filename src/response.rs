f3298b14ccef92eca0798fb473488359e30794ed
Tom Houlé <tom@tomhoule.com>
2018-04-14 15:00:03 +0200

    Implement nested object responses

--- src/response.rs
+++ src/response.rs
@@ -6,114 +6,78 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 
 pub trait PathFragment {
-    fn as_path_fragment() -> &'static str;
+    fn as_path_fragment(&self) -> &'static str;
 }
 
-enum ResponseNodeValue<Error> {
-    Unset,
-    Immediate(json::Value),
-    Delayed(Box<Future<Item = json::Value, Error = Error>>),
-}
-
-pub struct ResponseNode<Error> {
-    value: ResponseNodeValue<Error>,
-    prefix: Vec<&'static str>,
-}
-
-struct ResponseRoot {}
-
-pub struct ResponseBuilder<Error> {
-    tree: ResponseNode<Error>,
-}
-
-impl<Error> ResponseBuilder<Error> {
-    pub fn new() -> ResponseBuilder<Error> {
-        ResponseBuilder {
-            tree: ResponseNode {
-                prefix: Vec::new(),
-                value: ResponseNodeValue::Immediate(json::Value::Null),
-            },
-        }
-    }
-}
-
-impl<Error> ResponseNode<Error> {
-    fn new(prefix: Vec<&'static str>) -> Self {
-        ResponseNode {
-            value: ResponseNodeValue::Unset,
-            prefix,
-        }
-    }
-
-    /// Sets the current scope value to the `value` argument.
-    pub fn set<S: Serialize>(&mut self, value: S) {
-        self.value = ResponseNodeValue::Immediate(
-            json::to_value(value).expect("value can be converted to JSON"),
-        );
-    }
-
-    /// Sets `key` to `value` in the current scope. If the current scope is not an object. it becomes one with only that key-value.
-    pub fn set_kv<S: Serialize>(key: &str, value: S) {
-        // match self.value {
-        //   ResponseNodeValue::Immediate(json_value) => {
-        //      match json_value {
-        //        json::Value::Object(obj) => {
-        //           ...insert
-        //        }
-        //        _ => { self.value = ResponseNodeValue::Immediate(json!({ [key]: value })),
-        //
-        //      }
-        //   }
-        //   ResponseNodeValue::Deferred(deferred_value) => { unimplemented!() }
-        // }
-        //
-        unimplemented!();
-    }
-
-    /// Registers
-    pub fn set_deferred<'a, Resource: Identifiable>(ids: impl AsRef<&'a [&'a str]>) {
-        unimplemented!();
+impl<'a, T: PathFragment> PathFragment for &'a T {
+    fn as_path_fragment(&self) -> &'static str {
+        (*self).as_path_fragment()
     }
 }
 
 pub struct Response<Error> {
     _err: ::std::marker::PhantomData<Error>,
     data_fields: Vec<Box<Future<Item = json::Value, Error = Error>>>,
-}
-
-pub trait Handler<T, Error>
-where
-    Error: Debug + PartialEq + 'static,
-{
-    fn handle(&self, arg: T) -> Box<Future<Item = json::Value, Error = Error>>;
-}
-
-impl<T, F, R, Error: Debug + PartialEq + 'static> Handler<T, Error> for F
-where
-    R: IntoFuture<Item = json::Value, Error = Error> + 'static,
-    F: Fn(T) -> R,
-{
-    fn handle(&self, arg: T) -> Box<Future<Item = json::Value, Error = Error>> {
-        Box::new(self(arg).into_future())
-    }
+    path: Vec<&'static str>,
 }
 
 impl<Error: Debug + PartialEq + 'static> Response<Error> {
     pub fn new() -> Response<Error> {
+        Response::create(&["data"])
+    }
+
+    pub fn create(prefix: &[&'static str]) -> Response<Error> {
         Response {
             _err: ::std::marker::PhantomData,
             data_fields: Vec::new(),
+            path: prefix.to_owned(),
         }
     }
 
-    pub fn on<T: PathFragment>(&mut self, field: Option<T>, handler: impl Handler<T, Error>) {
+    pub fn on<T: PathFragment, F: IntoFuture<Item = json::Value, Error = Error> + 'static>(
+        mut self,
+        field: T,
+        handler: impl Fn(&T, Response<Error>) -> F,
+    ) -> Self {
+        let path_suffix = field.as_path_fragment();
+        let mut full_path = self.path.clone();
+        full_path.push(path_suffix);
+        self.data_fields.push(Box::new(
+            handler(&field, Response::create(&full_path)).into_future(),
+        ));
+        self
+    }
+
+    pub fn maybe_on<T: PathFragment, F: IntoFuture<Item = json::Value, Error = Error> + 'static>(
+        mut self,
+        field: Option<T>,
+        handler: impl Fn(&T, Response<Error>) -> F,
+    ) -> Self {
         if let Some(field) = field {
-            self.data_fields.push(handler.handle(field));
+            let path_suffix = field.as_path_fragment();
+            let mut full_path = self.path.clone();
+            full_path.push(path_suffix);
+            self.data_fields.push(Box::new(
+                handler(&field, Response::create(&full_path))
+                    .into_future()
+                    .map(move |res| json!({ path_suffix: res })),
+            ));
         }
+        self
+    }
+
+    pub fn merge(mut self, value: json::Value) -> Self {
+        self.data_fields
+            .push(Box::new(::futures::future::ok(value)));
+        self
     }
 
     pub fn resolve(self) -> impl Future<Item = json::Value, Error = Error> {
-        ::futures::future::join_all(self.data_fields).and_then(|data_fields| {
+        let Response {
+            path, data_fields, ..
+        } = self;
+
+        ::futures::future::join_all(data_fields).and_then(move |data_fields| {
             let mut data_map = HashMap::new();
 
             for field in data_fields.iter() {
@@ -122,7 +86,9 @@ impl<Error: Debug + PartialEq + 'static> Response<Error> {
                 }
             }
 
-            Ok(json!({ "data": data_map }))
+            let key: &'static str = path[path.len() - 1];
+
+            Ok(json!({ key: data_map }))
         })
     }
 }
@@ -131,31 +97,66 @@ impl<Error: Debug + PartialEq + 'static> Response<Error> {
 mod tests {
     use super::*;
 
-    enum QueryRoot {
-        Cats { name: String },
+    #[derive(Debug, PartialEq)]
+    enum DogSkills {
+        CanJump,
+        CanPlayDead,
+        CanSwim,
+        CanSit,
     }
 
-    impl PathFragment for QueryRoot {
-        fn as_path_fragment() -> &'static str {
-            "QueryRoot"
+    #[derive(Debug, PartialEq)]
+    enum Dog {
+        Name,
+        Age,
+        Fluffiness,
+        Skills { selection: Vec<DogSkills> },
+    }
+
+    #[derive(Debug, PartialEq)]
+    enum SomeField {
+        Cats {
+            name: String,
+        },
+        Dogs {
+            selection: Vec<Dog>,
+            puppies_only: bool,
+        },
+    }
+
+    impl PathFragment for Dog {
+        fn as_path_fragment(&self) -> &'static str {
+            match self {
+                Dog::Name => "name",
+                Dog::Age => "age",
+                Dog::Fluffiness => "fluffiness",
+                Dog::Skills { .. } => "skills",
+            }
+        }
+    }
+
+    impl PathFragment for SomeField {
+        fn as_path_fragment(&self) -> &'static str {
+            match self {
+                SomeField::Cats { .. } => "cats",
+                SomeField::Dogs { .. } => "dogs",
+            }
         }
     }
 
     #[derive(Debug, PartialEq)]
     struct Error;
 
-    fn resolve_query_root(q: QueryRoot) -> Result<json::Value, Error> {
+    fn resolve_query_root(q: &SomeField, res: Response<Error>) -> Result<json::Value, Error> {
         Ok(json!({ "weight": 4 }))
     }
 
     #[test]
     fn basic_json_object() {
-        let cats_request = QueryRoot::Cats {
+        let cats_request = SomeField::Cats {
             name: "Leopold".to_string(),
         };
-        let mut response = Response::<Error>::new();
-
-        response.on(Some(cats_request), resolve_query_root);
+        let mut response = Response::<Error>::new().on(cats_request, resolve_query_root);
 
         assert_eq!(
             ::futures::executor::block_on(response.resolve()).unwrap(),
@@ -165,17 +166,75 @@ mod tests {
 
     #[test]
     fn basic_future() {
-        let cats_request = QueryRoot::Cats {
+        let cats_request = SomeField::Cats {
             name: "Leopold".to_string(),
         };
-        let mut response = Response::<Error>::new();
-
-        response.on(Some(cats_request), |_req| {
+        let mut response = Response::<Error>::new().on(cats_request, |_req, _res| {
             ::futures::future::ok(json!({ "weight": 5 } ))
         });
 
         let resolved = ::futures::executor::block_on(response.resolve()).unwrap();
 
         assert_eq!(resolved, json!({ "data": { "weight": 5 } }));
+    }
+
+    #[test]
+    fn nested_json_object() {
+        let dogs_request = SomeField::Dogs {
+            selection: vec![
+                Dog::Name,
+                Dog::Fluffiness,
+                Dog::Skills {
+                    selection: vec![DogSkills::CanJump],
+                },
+            ],
+            puppies_only: false,
+        };
+
+        let response = Response::<Error>::new().on(dogs_request, |req, res| match req {
+            SomeField::Dogs {
+                selection,
+                puppies_only,
+            } => {
+                let default_response = json!({
+                    "name": "Laika",
+                    "age": 2,
+                    "unrelated_field": [3, 4, 5],
+                });
+
+                let skills_field = selection
+                    .iter()
+                    .filter_map(|dog| {
+                        if let Dog::Skills { .. } = dog {
+                            Some(dog)
+                        } else {
+                            None
+                        }
+                    })
+                    .next();
+
+                res.maybe_on(skills_field, |req, res| {
+                    Ok(json!({ "can_jump": true, "can_sit": true }))
+                }).merge(default_response)
+                    .resolve()
+            }
+            SomeField::Cats { name } => unimplemented!(),
+        });
+
+        let resolved = ::futures::executor::block_on(response.resolve()).unwrap();
+
+        assert_eq!(
+            resolved,
+            json!({
+                "data": {
+                    "dogs": {
+                        "name": "Laika",
+                        "age": 2,
+                        "unrelated_field": [3, 4, 5],
+                        "skills": { "can_jump": true, "can_sit": true },
+                    }
+                }
+            })
+        );
     }
 }
