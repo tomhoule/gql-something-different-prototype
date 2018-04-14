c8cb72758338478419bf367344dff55085135b1e
Tom Houlé <tom@tomhoule.com>
2018-04-14 11:13:42 +0200

    Implement basic async handler

--- src/response.rs
+++ src/response.rs
@@ -3,6 +3,7 @@ use identifiable::Identifiable;
 use json;
 use serde::*;
 use std::collections::HashMap;
+use std::fmt::Debug;
 
 pub trait PathFragment {
     fn as_path_fragment() -> &'static str;
@@ -77,10 +78,27 @@ impl<Error> ResponseNode<Error> {
 
 pub struct Response<Error> {
     _err: ::std::marker::PhantomData<Error>,
-    data_fields: Vec<json::Value>,
+    data_fields: Vec<Box<Future<Item = json::Value, Error = Error>>>,
 }
 
-impl<Error> Response<Error> {
+pub trait Handler<T, Error>
+where
+    Error: Debug + PartialEq + 'static,
+{
+    fn handle(&self, arg: T) -> Box<Future<Item = json::Value, Error = Error>>;
+}
+
+impl<T, F, R, Error: Debug + PartialEq + 'static> Handler<T, Error> for F
+where
+    R: IntoFuture<Item = json::Value, Error = Error> + 'static,
+    F: Fn(T) -> R,
+{
+    fn handle(&self, arg: T) -> Box<Future<Item = json::Value, Error = Error>> {
+        Box::new(self(arg).into_future())
+    }
+}
+
+impl<Error: Debug + PartialEq + 'static> Response<Error> {
     pub fn new() -> Response<Error> {
         Response {
             _err: ::std::marker::PhantomData,
@@ -88,21 +106,24 @@ impl<Error> Response<Error> {
         }
     }
 
-    pub fn on<T: PathFragment>(&mut self, field: Option<T>, handler: impl Fn(T) -> json::Value) {
+    pub fn on<T: PathFragment>(&mut self, field: Option<T>, handler: impl Handler<T, Error>) {
         if let Some(field) = field {
-            self.data_fields.push(handler(field));
+            self.data_fields.push(handler.handle(field));
         }
     }
 
-    pub fn resolve(&self) -> json::Value {
-        let mut data_map = HashMap::new();
+    pub fn resolve(self) -> impl Future<Item = json::Value, Error = Error> {
+        ::futures::future::join_all(self.data_fields).and_then(|data_fields| {
+            let mut data_map = HashMap::new();
 
-        for field in &self.data_fields {
-            if let json::Value::Object(map) = field {
-                data_map.extend(map.iter())
+            for field in data_fields.iter() {
+                if let json::Value::Object(map) = field {
+                    data_map.extend(map.iter())
+                }
             }
-        }
-        json!({ "data": data_map })
+
+            Ok(json!({ "data": data_map }))
+        })
     }
 }
 
@@ -120,7 +141,12 @@ mod tests {
         }
     }
 
+    #[derive(Debug, PartialEq)]
     struct Error;
+
+    fn resolve_query_root(q: QueryRoot) -> Result<json::Value, Error> {
+        Ok(json!({ "weight": 4 }))
+    }
 
     #[test]
     fn basic_json_object() {
@@ -129,8 +155,27 @@ mod tests {
         };
         let mut response = Response::<Error>::new();
 
-        response.on(Some(cats_request), |_req| json!({ "weight": 4 }));
+        response.on(Some(cats_request), resolve_query_root);
 
-        assert_eq!(response.resolve(), json!({ "data": { "weight": 4 } }));
+        assert_eq!(
+            ::futures::executor::block_on(response.resolve()).unwrap(),
+            json!({ "data": { "weight": 4 } })
+        );
+    }
+
+    #[test]
+    fn basic_future() {
+        let cats_request = QueryRoot::Cats {
+            name: "Leopold".to_string(),
+        };
+        let mut response = Response::<Error>::new();
+
+        response.on(Some(cats_request), |_req| {
+            ::futures::future::ok(json!({ "weight": 5 } ))
+        });
+
+        let resolved = ::futures::executor::block_on(response.resolve()).unwrap();
+
+        assert_eq!(resolved, json!({ "data": { "weight": 5 } }));
     }
 }
