cb1c987bd27462fef296c2c9da14e8c709ee2aed
Tom Houlé <tom@tomhoule.com>
2018-04-20 07:28:15 +0200

    Add non-functional echo server implementation
    Made me realise introspection is not implemented yet, and it needs to
    be.

---
+++ examples/echo-server/src/main.rs
@@ -0,0 +1,72 @@
+#[macro_use]
+extern crate tokio_gql;
+#[macro_use]
+extern crate serde_json;
+extern crate futures;
+
+use futures::prelude::*;
+use serde_json as json;
+use tokio_gql::response::{leaf, rest, Response};
+
+pub mod schema {
+    #[derive(SomethingCompletelyDifferent)]
+    #[SomethingCompletelyDifferent(path = "src/schema.graphql")]
+    struct EchoSchema;
+}
+
+struct EchoResolver {
+    archive: Vec<String>,
+}
+
+#[derive(PartialEq, Debug)]
+struct Error;
+
+impl tokio_gql::service::GqlService for EchoResolver {
+    type Schema = schema::Schema;
+    type Error = Error;
+
+    fn handle_errors(&self, errors: tokio_gql::errors::GqlError<Self::Error>) -> json::Value {
+        json!({})
+    }
+
+    fn resolve(
+        &self,
+        request: Self::Schema,
+        response: Response<Error>,
+    ) -> Box<Future<Item = json::Value, Error = Self::Error>> {
+        let query_response = response.on(request.query, |req, _res| match req {
+            schema::EchoQuery::PastEchoes => leaf(req, Ok(json!(self.archive))),
+        });
+
+        let mutation_response =
+            tokio_gql::response::Response::new().on(request.mutation, |req, res| match req {
+                schema::EchoMutation::Echo { message } => {
+                    rest(Ok(json::Value::String(match message {
+                        Some(msg) => msg.to_string(),
+                        None => "this probably should reply with an empty string".to_string(),
+                    })))
+                }
+            });
+
+        Box::new(
+            query_response
+                .into_future()
+                .join(mutation_response)
+                .map(|(q, m)| {
+                    json!({
+                        "data": {
+                            "query": q,
+                            "mutation": m,
+                        }
+                    })
+                }),
+        )
+    }
+}
+
+fn main() {
+    tokio_gql::standalone_server::StandaloneServer::new(EchoResolver {
+        archive: Vec::new(),
+    }).start()
+        .unwrap()
+}
--- src/standalone_server.rs
+++ src/standalone_server.rs
@@ -40,22 +40,37 @@ where
 
     fn call(&self, req: Self::Request) -> Self::Future {
         let cloned = self.0.clone();
-        Box::new(
-            body_to_response(self.0.clone(), req.body())
-                .and_then(|body| {
-                    let mut res = hyper::Response::new();
-                    res.set_body(json::to_string(&body).expect("the response is valid json"));
-                    Ok(res)
-                })
-                .or_else(move |err| {
-                    let mut res = hyper::Response::new();
-                    let resolver: &Resolver = &cloned.resolver;
-                    let body_string = json::to_string(&resolver.handle_errors(err)).unwrap();
-                    res.set_body(body_string);
-                    Ok(res)
-                }),
-        )
+
+        match (req.uri().path(), req.method()) {
+            ("/graphql", hyper::Method::Get) => Box::new(playground().into_future()),
+            ("/graphql", hyper::Method::Post) => Box::new(
+                body_to_response(self.0.clone(), req.body())
+                    .and_then(|body| {
+                        let mut res = hyper::Response::new();
+                        res.set_body(json::to_string(&body).expect("the response is valid json"));
+                        Ok(res)
+                    })
+                    .or_else(move |err| {
+                        let mut res = hyper::Response::new();
+                        let resolver: &Resolver = &cloned.resolver;
+                        let body_string = json::to_string(&resolver.handle_errors(err)).unwrap();
+                        res.set_body(body_string);
+                        Ok(res)
+                    }),
+            ),
+            (_, _) => Box::new(redirect().into_future()),
+        }
     }
+}
+
+fn redirect() -> Result<hyper::Response, hyper::Error> {
+    let mut res = hyper::Response::new();
+    res.set_status(hyper::StatusCode::SeeOther);
+    {
+        let headers = res.headers_mut();
+        headers.set_raw("Location", "/graphql");
+    }
+    Ok(res)
 }
 
 fn body_to_response<
@@ -90,6 +105,17 @@ fn body_to_response<
         .map(|_| json!({}))
 }
 
+fn playground() -> Result<hyper::Response, hyper::Error> {
+    let template = include_str!("graphql_playground.html");
+    let mut res = hyper::Response::new();
+    res.set_body(template);
+    {
+        let headers = res.headers_mut();
+        headers.set_raw("Content-Type", "text/html");
+    }
+    Ok(res)
+}
+
 pub struct StandaloneServer<Schema, Error, Resolver>
 where
     Schema: CoerceQueryDocument,
@@ -112,10 +138,12 @@ where
     pub fn start(self) -> Result<(), ()> {
         let new_service = ::std::sync::Arc::new(self);
         hyper::server::Http::new()
-            .bind(&"8000".parse().unwrap(), move || {
+            .bind(&"127.0.0.1:8000".parse().unwrap(), move || {
                 Ok(ServerWrapper(new_service.clone()))
             })
-            .expect("server started");
+            .expect("bound to localhost:8000")
+            .run()
+            .unwrap();
         Ok(())
     }
 }
